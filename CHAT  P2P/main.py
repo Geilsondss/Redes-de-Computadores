585f7e5c6326cc0e9399075030d8bed5e3fb7ed0
Geilson Sá <geilsondossantos098@gmail.com>
2025-06-29 16:23:28 -0300

    Atualização de comandos

--- CHAT  P2P/main.py
+++ CHAT  P2P/main.py
@@ -16,7 +16,6 @@ Thread(target=servidor.start, daemon=True).start()
 comandos = {
     '/connect': lambda e: cliente.connect(socket_to_tuple(e.split()[1]), obter_hostname(PORTA)),
     '/peers': lambda e: print(peersdb.peers),
-    '/connections': lambda e: print(cliente.connections),
     '/resignin': lambda e: usuario.signin(),
     '/create_room': lambda e: print(
         "<SISTEMA>: É necessário fornecer nome, porta e senha." if len(e.split()) < 4
@@ -26,13 +25,26 @@ comandos = {
             senha=e.split()[3],
             criador=str(usuario))
     ),
-    '/disconnect': lambda e: cliente.disconnect(e.split()[1]),
-    '/add_in_room' : lambda e: cliente.connect(socket_to_tuple(e.split()[1]), obter_hostname(PORTA)),
     '/enter_room': lambda e: entrar_na_sala(e),
-    '/kick_peer': lambda e: cliente.disconnect(e.split()[1]),
     '/clear': lambda e: clear(),
-    '/menu': lambda e: mostrar_comandos()
+    '/menu': lambda e: mostrar_comandos(),
+    '/disconnect': lambda e: cliente.disconnect(e.split()[1]),
+    '/add_in_room' : lambda e: (
+        cliente.connect(socket_to_tuple(e.split()[1]), obter_hostname(PORTA)),
+        cliente.send_control_message(e.split()[1], "__ADDED_TO_ROOM__")
+    ),
+    '/kick_peer': lambda e: (
+        cliente.send_control_message(e.split()[1], "__KICK__"),
+        cliente.disconnect(e.split()[1])
+    ),
+    '/rooms': lambda e: print(salasdb.listar_salas()),
+    '/leave_room': lambda e: print(salasdb.sair_sala(str(usuario))),
+    '/delete_room': lambda e: print(
+        "<SISTEMA>: É necessário fornecer o nome da sala." if len(e.split()) < 2
+        else salasdb.deletar_sala(e.split()[1], str(usuario))
+    ),
 }
+
 
 mostrar_comandos()
 
